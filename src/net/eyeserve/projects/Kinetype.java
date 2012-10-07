c9926cf7e7e20b1fb909633bbb38a1b6d2681ceb
Jordan Donaldson <jordan@jordohio.net>
2012-10-07 14:40:00 -0400

    Original release.
    ALPHA 0.1 WOO!

---
+++ src/net/eyeserve/projects/Kinetype.java
@@ -0,0 +1,14 @@
+package net.eyeserve.projects;
+
+public class Kinetype {
+	
+	//creates an instance of kinetypeGUI in a static manner so that it can be
+	//referred to from any class
+	public static KinetypeGUI kinetypeGUI;
+	
+	public static void main(String[] args)
+	{
+		//defines that instance
+		kinetypeGUI = new KinetypeGUI();
+	}
+}
